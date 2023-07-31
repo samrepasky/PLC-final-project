85345869546f678c004bc8f4e6c73857ceedcaac
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-07-31 00:10:24 -0400

    Added gravity to the cube (still not moving)

--- src/pages/components1/cube.jsx
+++ src/pages/components1/cube.jsx
@@ -1,10 +1,18 @@
 import React, { Component, useRef, useState } from 'react';
 import { Canvas, useFrame } from "@react-three/fiber";
 
+const GRAVITY = -9.81;
 
 function Box(props) {
     const mesh = useRef();
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+    const [velocity, setVelocity] = useState([0, 0, 0]);
+
+    useFrame(() => {
+        const newVelocity = velocity.map((v, i) => v + GRAVITY * Math.abs(v));
+        setVelocity(newVelocity);
+        mesh.current.position.y += velocity[1];
+    });
+
     return (
       
        <mesh {...props} ref={mesh}>
